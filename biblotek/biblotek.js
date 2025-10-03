02a3cfffb4bc301ba4cf556b79b29b97a186f6a1
Mosho <moises.leon@chasacademy.se>
2025-10-03 15:48:27 +0200

    added api

--- biblotek/biblotek.js
+++ biblotek/biblotek.js
@@ -2,18 +2,34 @@
  * @Author: MoS 
  * @Date: 2025-10-03 11:06:57 
  * @Last Modified by: MoS
- * @Last Modified time: 2025-10-03 11:58:55
+ * @Last Modified time: 2025-10-03 15:47:18
  */
 
+const apikey = "AIzaSyDBIO4OfNn2JsHWIklCuiLqCHVLre-jOck"
 
 const booksResults = document.getElementById("booksResults")
 const searchInput = document.getElementById("search")
 
+//validerar: om tom? sök efter "frogs"
 if(searchInput == ""){
-    searchInput.textContent = "Frogs"
-} else {
-    
+    const startQuery = "Frogs"
 }
 
+async function GoogleBooksAPI () {
+    const getGoogleBooksAPI = await fetch(`https://www.googleapis.com/books/v1/volumes?q=frogs&maxResults=5&startIndex=10`)
+    const GoogleBooksData = await getGoogleBooksAPI.json()
+ 
+    //Testar att printa namnet på första Item
+    //console.log(GoogleBooksData.items[0].volumeInfo.title)
 
-const googleBooksAPI = `https://www.googleapis.com/books/v1/volumes?q=${}frogs&maxResults=5&startIndex=10`
+    for (let book of GoogleBooksData.items) {
+        const bookTitle = document.createElement("p")
+        bookTitle.classList.add("bookTitle")
+        bookTitle.textContent = volumeInfo.title
+
+        booksResults.appendChild(bookTitle)
+    }
+}
+
+//Anropar functionen
+GoogleBooksAPI()
