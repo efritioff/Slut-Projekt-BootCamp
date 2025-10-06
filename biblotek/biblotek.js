593a2f12d344e8510572815e10ae30b30b3b9c3a
Mosho <moises.leon@chasacademy.se>
2025-10-06 08:10:07 +0200

    listar böckerna

--- biblotek/biblotek.js
+++ biblotek/biblotek.js
@@ -2,7 +2,7 @@
  * @Author: MoS 
  * @Date: 2025-10-03 11:06:57 
  * @Last Modified by: MoS
- * @Last Modified time: 2025-10-03 15:47:18
+ * @Last Modified time: 2025-10-06 08:06:12
  */
 
 const apikey = "AIzaSyDBIO4OfNn2JsHWIklCuiLqCHVLre-jOck"
@@ -11,23 +11,35 @@ const booksResults = document.getElementById("booksResults")
 const searchInput = document.getElementById("search")
 
 //validerar: om tom? sök efter "frogs"
-if(searchInput == ""){
-    const startQuery = "Frogs"
+if(searchInput === ""){
+    startQuery.textContent = "Frogs"
 }
 
 async function GoogleBooksAPI () {
-    const getGoogleBooksAPI = await fetch(`https://www.googleapis.com/books/v1/volumes?q=frogs&maxResults=5&startIndex=10`)
+    const getGoogleBooksAPI = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}&maxResults=5&startIndex=10`)
     const GoogleBooksData = await getGoogleBooksAPI.json()
  
     //Testar att printa namnet på första Item
     //console.log(GoogleBooksData.items[0].volumeInfo.title)
 
     for (let book of GoogleBooksData.items) {
+
+        const bookHolder = document.createElement("div")
+        bookHolder.classList.add("bookHolder")
+        
+        const bookImg = document.createElement("img")
+        bookImg.src = book.volumeInfo.imageLinks.thumbnail
+        bookImg.alt = `Denna bok heter ${book.volumeInfo.title}`
+
+
         const bookTitle = document.createElement("p")
         bookTitle.classList.add("bookTitle")
-        bookTitle.textContent = volumeInfo.title
+        bookTitle.textContent = book.volumeInfo.title
 
-        booksResults.appendChild(bookTitle)
+        bookHolder.appendChild(bookImg)
+        bookHolder.appendChild(bookTitle)
+
+        booksResults.appendChild(bookHolder)
     }
 }
 
